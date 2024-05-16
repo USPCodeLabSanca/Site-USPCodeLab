7a092d8942308e8ae1a305cf0e20a348081a9de3
Gabriel Barbosa <50121055+GotemBarbosa@users.noreply.github.com>
2024-05-16 14:31:27 -0300

    estrutura card "ajustes"

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -8,9 +8,12 @@ module.exports = {
     extend: {
       colors: {
         'primary': '#53B39C',
+<<<<<<< HEAD
         'primaryDark': '#3D8574',
         'primaryDarker': '#30685B',
         'primaryLight': '#6FF2D3',
+=======
+>>>>>>> e313823 ( estrutura card)
         'background': "#292524",
         'grayBackground': "#464646",
         'lightGrayBackground': '#D9D9D9',
