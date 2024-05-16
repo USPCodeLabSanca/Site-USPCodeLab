4c1c04ac382b149c644d4498b9b3cd51606b74b8
Gabriel Barbosa <50121055+GotemBarbosa@users.noreply.github.com>
2024-05-16 09:43:51 -0300

    pastas adicionadas + ajustes conflito

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -1,9 +1,22 @@
+import { Routes, Route } from "react-router-dom";
+import {Homepage, Projects, Events} from "src/pages";
 
 
-export default function Routes(){
+function Routes(){
     return(
-        <div>
-            
-        </div>
+        <>
+        <Routes>
+            <Route path="/" 
+            element={ <Homepage/>} // inserir paginas aqui
+            />
+
+            <Route path="/projects"
+            element={<Projects/>}
+            />
+
+            <Route path="/events"
+            element={<Events/>}/>
+        </Routes>
+        </>
     )
 }
