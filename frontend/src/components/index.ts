60d64b49d452511b7539ec9ff4ffc710e6b0cb6b
joseCarlosAndrade <josecarlos.101@usp.br>
2024-05-17 21:33:13 -0300

    resolving import project card

--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -1,4 +1,4 @@
 export{ default as Button } from './Button/Button';
-export {default as ProjectCard} from "./ProjectCard";
+export {default as ProjectCard} from "./ProjectCard/ProjectCard";
 export { default as Footer} from './Footer/Footer';
 
