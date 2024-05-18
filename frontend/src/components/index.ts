4fb5bf1937de736f3d412a94635220348bb40de1
José Carlos Andrade do Nascimento <89979529+joseCarlosAndrade@users.noreply.github.com>
2024-05-17 21:23:09 -0300

    erro de merge comentado

--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -1,5 +1,2 @@
-<<<<<<< HEAD
-export { default as Button } from './Button/Button';
-=======
+export{ default as Button } from './Button/Button';
 export {default as ProjectCard} from "./ProjectCard";
->>>>>>> e313823 ( estrutura card)
