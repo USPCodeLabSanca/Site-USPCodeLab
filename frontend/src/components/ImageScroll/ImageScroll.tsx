a08950e488791b5842a9e39027577be7cc1f93cf
joseCarlosAndrade <josecarlos.101@usp.br>
2024-05-19 23:11:51 -0300

    vertical scrolling done

---
+++ frontend/src/components/ImageScroll/ImageScroll.tsx
@@ -0,0 +1,124 @@
+import React, { useEffect, useState } from 'react'
+import Scrollable from './Scrollable/Scrollable';
+import userInterval, { useInterval } from 'src/utils';
+
+import { SVGType } from 'src/types';
+
+// type ScrollableType = typeof Scrollable;
+
+interface ImageScrollProps {
+    images : React.FC[] | SVGType[]
+}
+
+// const images = [0, 1, 2, 3, 4, 5]
+
+
+const ImageScroll : React.FC<ImageScrollProps> = (props : ImageScrollProps) => {
+    const imageArray = props.images;
+
+    const [imageIndex, setImageIndex] = useState(0);
+    const [nextImageIndex, setNextImageIndex] = useState(1);
+
+    const [index, setIndex] = useState(imageIndex); // index conteudo da imagem atual
+    const [nextIndex, setNextIndex] = useState(nextImageIndex); // index do conteudo da proxima imagem
+    const [tempIndex, setTempIndex] = useState(3); //  index temporario
+
+    const [i, setI] = useState(0);
+
+    const [ itensTranlate, setItensTranslate] = useState([1, 1, 1])
+    
+
+    const updateImageIndex  = () => {
+
+        setImageIndex( imageIndex === imageArray.length-1 ? 0 : imageIndex + 1 );
+        setNextImageIndex( nextImageIndex === imageArray.length-1 ? 0 : nextImageIndex + 1 )
+    }
+
+    
+
+    const moveDown = () => { // move para baixo
+       
+        updateImageIndex();
+        let new_i = 0;
+
+        console.log(i)
+        switch (i) {
+            case 0: // temp index invisivel - 1
+                setItensTranslate([-1, 2, 2])
+                // console.log('depois3: ' , i)
+                setI(1);
+                setTempIndex(nextImageIndex)
+                new_i = 1;
+                break;
+
+            case 1: // index invisivel - 3
+                setItensTranslate([0, 0, 3])
+                // console.log('depoisxxx: ' , i)
+                setI(2);
+                setIndex(nextImageIndex);
+                new_i = 2;
+                break;
+
+            case 2: // nextindex invisivel - 0
+                setItensTranslate([1, 1, 1])
+                // console.log('depois2: ' , i)
+                setI(0);
+                setNextIndex(nextImageIndex)
+                new_i = 0;
+                break;
+        }
+        return new_i;
+
+    }
+
+    // useEffect(()=> {
+    //     const interval = setInterval(() => {
+    //         const new_i = moveDown();
+    //         setI(new_i)
+    //     }, 3000);
+
+    //     return () => clearInterval(interval);
+    // }, []);
+
+    // const rendering = [images[images.length-1], images[0], images[1]];
+    useInterval(moveDown, 2000);
+  
+    return (
+      <>
+      <div className='max-h-96 max-w-96 h-96 w-full overflow-hidden'>
+
+         (
+            <div 
+                className={` ${(i ===1) ? "invisible" : "visible"} flex transition-transform ease-out duration-500`}
+                style={{ transform: `translateY(${-1*itensTranlate[0] * 100}%)` }} >
+                    <Scrollable image={imageArray[tempIndex]} n={tempIndex}></Scrollable>
+            </div>
+
+            <div 
+                className={`${(i ===2) ? "invisible" : "visible"} flex transition-transform ease-out duration-500`}
+                style={{ transform: `translateY(-${(itensTranlate[1]) * 100}%)` }} >
+                    <Scrollable image={imageArray[index]} n={index}></Scrollable>
+            </div>
+
+            <div 
+                className={`${(i ===0) ? "invisible" : "visible"} flex transition-transform ease-out duration-500`}
+                style={{ transform: `translateY(-${(itensTranlate[2]) * 100}%)` }} >
+                    <Scrollable image={imageArray[nextIndex]} n={nextIndex}></Scrollable>
+            </div>
+        )
+    
+
+      </div>
+
+      {/* <button className='bg-white' onClick={moveDown}>next</button> */}
+      </>
+      
+    )
+  
+}
+
+export default ImageScroll;
+/*
+
+
+*/
