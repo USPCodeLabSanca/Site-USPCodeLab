9c72389a98d3c8d80336c3ef63782c24a313c552
rafaelalmeida9 <rafaeldealmeida@usp.br>
2024-05-31 00:15:51 -0300

    header done

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,12 @@
 import Routes from "./routes"
-
+import { Header } from 'src/components'
 
 function App() {
 
   return (
     <div>
       <div className="w-screen h-screen bg-background overflow-x-hidden overflow-y-auto">
+        <Header />
         <Routes />
       </div>
     </div>
