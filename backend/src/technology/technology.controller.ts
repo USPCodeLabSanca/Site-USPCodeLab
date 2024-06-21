43bc4667d10eb843e27cd1ebe83f95efd47d5d59
Yuri Faria <yuri.yurifaria@gmail.com>
2024-06-21 20:17:13 -0300

    feat: create technology endpoint

--- backend/src/technology/technology.controller.ts
+++ backend/src/technology/technology.controller.ts
@@ -1,12 +1,15 @@
 import { Request, Response } from 'express'
 import { getTechnologies } from './controllers/getTechnologies'
+import { createTechnology } from './controllers/createTechnology'
 
 interface Controller {
   getTechnologies: (req: Request, res: Response) => void
+  createTechnology: (req: Request, res: Response) => void
 }
 
 const controller: Controller = {
   getTechnologies,
+  createTechnology,
 }
 
 export default controller
--- backend/src/technology/technology.router.ts
+++ backend/src/technology/technology.router.ts
@@ -4,5 +4,6 @@ import controller from './technology.controller'
 const router = express.Router()
 
 router.route('/').get(controller.getTechnologies)
+router.route('/').post(controller.createTechnology)
 
 export default router
