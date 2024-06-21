0c908fe5bd7ffd676372b3de3370c25d74a47f55
Yuri Faria <yuri.yurifaria@gmail.com>
2024-06-21 20:21:09 -0300

    feat: delete technology endpoint

--- backend/src/technology/technology.controller.ts
+++ backend/src/technology/technology.controller.ts
@@ -1,15 +1,18 @@
 import { Request, Response } from 'express'
 import { getTechnologies } from './controllers/getTechnologies'
 import { createTechnology } from './controllers/createTechnology'
+import { deleteTechnology } from './controllers/deleteTechnology'
 
 interface Controller {
   getTechnologies: (req: Request, res: Response) => void
   createTechnology: (req: Request, res: Response) => void
+  deleteTechnology: (req: Request, res: Response) => void
 }
 
 const controller: Controller = {
   getTechnologies,
   createTechnology,
+  deleteTechnology,
 }
 
 export default controller
