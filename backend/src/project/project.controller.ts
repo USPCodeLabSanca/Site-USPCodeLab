b4126397db2b1b6e83874fe0f5a5ba39a2a62663
Yuri Faria <yuri.yurifaria@gmail.com>
2024-06-21 20:59:33 -0300

    feat: update project endpoint

--- backend/src/project/project.controller.ts
+++ backend/src/project/project.controller.ts
@@ -1,15 +1,18 @@
 import { Request, Response } from 'express'
 import { getProjects } from './controllers/getProjects'
 import { createProject } from './controllers/createProject'
+import { updateProject } from './controllers/updateProject'
 
 interface Controller {
   getProjects: (req: Request, res: Response) => void
   createProject: (req: Request, res: Response) => void
+  updateProject: (req: Request, res: Response) => void
 }
 
 const controller: Controller = {
   getProjects,
   createProject,
+  updateProject,
 }
 
 export default controller
