ffe3344d02f5979eedd6ead72cf1faeed51ff787
Yuri Faria <yuri.yurifaria@gmail.com>
2024-06-21 20:34:59 -0300

    feat: update group endpoint

--- backend/src/group/group.router.ts
+++ backend/src/group/group.router.ts
@@ -5,5 +5,6 @@ const router = express.Router()
 
 router.route('/').get(controller.getGroups)
 router.route('/').post(controller.createGroup)
+router.route('/:id').patch(controller.updateGroup)
 
 export default router
