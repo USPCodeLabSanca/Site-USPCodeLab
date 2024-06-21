005ddf66ad9ddcdadb870d63f8a93476e0906a9e
Yuri Faria <yuri.yurifaria@gmail.com>
2024-06-21 20:30:36 -0300

    feat: get all groups endpoint

--- backend/src/router/index.ts
+++ backend/src/router/index.ts
@@ -1,9 +1,11 @@
 import express from 'express'
 
 import technologyRouter from '../technology/technology.router'
+import groupRouter from '../group/group.router'
 
 const router = express.Router()
 
 router.use('/technology', technologyRouter)
+router.use('/group', groupRouter)
 
 export default router
