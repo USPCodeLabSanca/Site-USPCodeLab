d902c277080fc704001744947f8dd33d1a9bdb78
Yuri Faria <yuri.yurifaria@gmail.com>
2024-06-21 20:59:33 -0300

    feat: update project endpoint

---
+++ backend/src/project/controllers/updateProject.ts
@@ -0,0 +1,29 @@
+import { Request, Response } from 'express'
+import prisma from '../../db'
+
+const updateProject = async (req: Request, res: Response) => {
+  try {
+    const id = Number(req.params.id)
+    const { name, description, github_link, date, photo, id_group } = req.body
+
+    const project = await prisma.project.findUnique({ where: { id_project: id } })
+
+    if (!project) return res.status(404).send({ error: 'Projeto não encontrado.' })
+
+    if (id_group) {
+      const group = await prisma.group.findUnique({ where: { id_group } })
+      if (!group) return res.status(404).send({ error: 'Grupo não encontrado.' })
+    }
+
+    const newProject = await prisma.project.update({
+      where: { id_project: id },
+      data: { name, description, github_link, date, photo, id_group },
+    })
+
+    return res.send(newProject)
+  } catch (error) {
+    res.status(500).send({ error: 'Erro ao atualizar projeto.' })
+  }
+}
+
+export { updateProject }
--- backend/src/project/project.router.ts
+++ backend/src/project/project.router.ts
@@ -5,5 +5,6 @@ const router = express.Router()
 
 router.route('/').get(controller.getProjects)
 router.route('/').post(controller.createProject)
+router.route('/:id').patch(controller.updateProject)
 
 export default router
