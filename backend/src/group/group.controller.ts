855fb609d113ef2c2de2a86f71f8447796858a9d
Yuri Faria <yuri.yurifaria@gmail.com>
2024-06-21 20:34:59 -0300

    feat: update group endpoint

--- backend/src/group/group.controller.ts
+++ backend/src/group/group.controller.ts
@@ -1,15 +1,18 @@
 import { Request, Response } from 'express'
 import { getGroups } from './controllers/getGroups'
 import { createGroup } from './controllers/createGroup'
+import { updateGroup } from './controllers/updateGroup'
 
 interface Controller {
   getGroups: (req: Request, res: Response) => void
   createGroup: (req: Request, res: Response) => void
+  updateGroup: (req: Request, res: Response) => void
 }
 
 const controller: Controller = {
   getGroups,
   createGroup,
+  updateGroup,
 }
 
 export default controller
