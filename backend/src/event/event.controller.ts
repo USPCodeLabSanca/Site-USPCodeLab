cb9119ff63e095ff3e67c0517c0d09dc81ffdc3e
Yuri Faria <yuri.yurifaria@gmail.com>
2024-06-21 20:45:23 -0300

    feat: create event endpoint

--- backend/src/event/event.controller.ts
+++ backend/src/event/event.controller.ts
@@ -1,12 +1,15 @@
 import { Request, Response } from 'express'
 import { getEvents } from './controllers/getEvents'
+import { createEvent } from './controllers/createEvent'
 
 interface Controller {
   getEvents: (req: Request, res: Response) => void
+  createEvent: (req: Request, res: Response) => void
 }
 
 const controller: Controller = {
   getEvents,
+  createEvent,
 }
 
 export default controller
