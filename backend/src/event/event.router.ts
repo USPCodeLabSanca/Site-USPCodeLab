b4d93ad947868e8e44d3414e0e819f2265db6843
Yuri Faria <yuri.yurifaria@gmail.com>
2024-06-21 20:45:23 -0300

    feat: create event endpoint

--- backend/src/event/event.router.ts
+++ backend/src/event/event.router.ts
@@ -4,5 +4,6 @@ import controller from './event.controller'
 const router = express.Router()
 
 router.route('/').get(controller.getEvents)
+router.route('/').post(controller.createEvent)
 
 export default router
