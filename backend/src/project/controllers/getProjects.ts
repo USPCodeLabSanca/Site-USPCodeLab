23b538ee16dc672ba0cab3b6405afd8e90b09b6b
Yuri Faria <yuri.yurifaria@gmail.com>
2024-06-21 21:52:36 -0300

    feat: change nn project endpoints

--- backend/src/project/controllers/getProjects.ts
+++ backend/src/project/controllers/getProjects.ts
@@ -3,7 +3,7 @@ import prisma from '../../db'
 
 const getProjects = async (req: Request, res: Response) => {
   try {
-    const projects = await prisma.project.findMany({ orderBy: { name: 'asc' } })
+    const projects = await prisma.project.findMany({ orderBy: { name: 'asc' }, include: { technologies: true } })
     return res.send(projects)
   } catch (error) {
     res.status(500).send({ error: 'Erro ao obter a lista de projetos cadastrados.' })
