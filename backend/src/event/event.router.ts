f98406bc6bce04aca13785aa880db53782a75565
Yuri Faria <yuri.yurifaria@gmail.com>
2024-06-21 21:59:48 -0300

    feat: auth

--- backend/src/event/event.router.ts
+++ backend/src/event/event.router.ts
@@ -1,11 +1,12 @@
 import express from 'express'
 import controller from './event.controller'
+import auth from '../middleware/auth'
 
 const router = express.Router()
 
 router.route('/').get(controller.getEvents)
-router.route('/').post(controller.createEvent)
-router.route('/:id').patch(controller.updateEvent)
-router.route('/:id').delete(controller.deleteEvent)
+router.route('/').all(auth).post(controller.createEvent)
+router.route('/:id').all(auth).patch(controller.updateEvent)
+router.route('/:id').all(auth).delete(controller.deleteEvent)
 
 export default router
