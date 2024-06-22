6201179742b2211e9df490ec87acd635e7dd5858
Yuri Faria <yuri.yurifaria@gmail.com>
2024-06-21 21:52:36 -0300

    feat: change nn project endpoints

--- backend/src/project/controllers/createProject.ts
+++ backend/src/project/controllers/createProject.ts
@@ -3,18 +3,35 @@ import prisma from '../../db'
 
 const createProject = async (req: Request, res: Response) => {
   try {
-    const { name, description, github_link, date, photo, id_group } = req.body
+    const { name, description, github_link, date, photo, id_group, technologies } = req.body
 
-    if (!name || !description || !github_link || !date || !id_group)
+    if (!name || !description || !github_link || !date || !id_group || !technologies)
       return res.status(400).send({ error: 'Nome, descrição, link do github e data são obrigatórios.' })
 
     const group = await prisma.group.findUnique({ where: { id_group } })
 
     if (!group) return res.status(404).send({ error: 'Grupo não encontrado.' })
 
-    const project = await prisma.project.create({ data: { name, description, github_link, date, photo, id_group } })
+    const project = await prisma.project.create({
+      data: {
+        name,
+        description,
+        github_link,
+        date,
+        photo,
+        id_group,
+        technologies: {
+          connect: [
+            ...(technologies || []).map((technologyId: number) => ({
+              id_technology: technologyId,
+            })),
+          ],
+        },
+      },
+    })
     return res.send(project)
   } catch (error) {
+    console.error(`INTERNAL ERROR CreateProject: ${error}`)
     res.status(500).send({ error: 'Erro ao cadastrar projeto.' })
   }
 }
--- backend/src/project/controllers/updateProject.ts
+++ backend/src/project/controllers/updateProject.ts
@@ -4,7 +4,7 @@ import prisma from '../../db'
 const updateProject = async (req: Request, res: Response) => {
   try {
     const id = Number(req.params.id)
-    const { name, description, github_link, date, photo, id_group } = req.body
+    const { name, description, github_link, date, photo, id_group, technologies } = req.body
 
     const project = await prisma.project.findUnique({ where: { id_project: id } })
 
@@ -17,11 +17,26 @@ const updateProject = async (req: Request, res: Response) => {
 
     const newProject = await prisma.project.update({
       where: { id_project: id },
-      data: { name, description, github_link, date, photo, id_group },
+      data: {
+        name,
+        description,
+        github_link,
+        date,
+        photo,
+        id_group,
+        technologies: {
+          connect: [
+            ...(technologies || []).map((technologyId: number) => ({
+              id_technology: technologyId,
+            })),
+          ],
+        },
+      },
     })
 
     return res.send(newProject)
   } catch (error) {
+    console.error(`INTERNAL ERROR UpdateProject: ${error}`)
     res.status(500).send({ error: 'Erro ao atualizar projeto.' })
   }
 }
