16f2c070c35c752078ec057607cd10ecfe081576
shogoshima <shogoshima@usp.br>
2024-09-17 22:38:41 -0300

    correcao card projeto para mostrar icones e deixar mais responsivo

--- frontend/src/components/Icon/Icon.tsx
+++ frontend/src/components/Icon/Icon.tsx
@@ -7,8 +7,9 @@ import { SVGType } from 'src/types'
 // Props do icone - Passar imagem e alt text como parametero. Style eh opcional.
 interface IconProps {
     // SVG?: SVGType,
-    // PNG? : React.FunctionComponent,
-    Image: SVGType | React.FunctionComponent,
+  // PNG? : React.FunctionComponent,
+    title: string,
+    Image: SVGType | React.FunctionComponent | string,
     alt : string,
     style? : string,
     link?: string // proval link associado
@@ -18,16 +19,16 @@ interface IconProps {
 const Icon : React.FC<IconProps> = ( props : IconProps)  => {
 
     // const Image : SVGType | React.FunctionComponent | undefined =  props.SVG ? props.SVG : props.PNG;
-    const Image = props.Image
+  const Image = props.Image
   return (
     <>
         {/* componente icone com estilizaçao padrao w-14 caso a prop style nao seja passada */}
         {props?.link ? 
           <a href={props.link} target="_blank" rel="noopener noreferrer">
-            <img className={props.style ? props.style : 'w-14'}  src={Image.toString()} alt={props.alt} />
+            <img className={props.style ? props.style : 'w-14'} title={props.title} src={Image.toString()} alt={props.alt} />
           </a>
           : 
-          <img className={props.style ? props.style : 'w-14'}  src={Image.toString()} alt={props.alt} />
+          <img className={props.style ? props.style : 'w-14'} title={props.title} src={Image.toString()} alt={props.alt} />
           }
         
 
--- frontend/src/components/ProjectCard/ProjectCard.tsx
+++ frontend/src/components/ProjectCard/ProjectCard.tsx
@@ -22,47 +22,61 @@ function ProjectCard(cardProps: ProjectCardProps) {
       className={`${cardProps.className} bg-grayBackground rounded-xl  flex  flex-col md:flex-row px-6 py-5 gap-4 md:justify-between`}
     >
       <div className="flex flex-col gap-1 md:justify-between md:gap-3 w-full">
-        <div className="flex flex-col lg:flex-row gap-2">
+        <div className="flex flex-col lg:flex-row gap-2 md:justify-between">
           <h2 className="font-monteserrat font-semibold text-white m-auto text-lg md:text-xl px-2 md:px-0 mr-4 ml-0 w-fit">
             {cardProps.title}
           </h2>
-          <div className="flex px-2 gap-2 md:gap-3">
+          <div className="flex gap-2 md:gap-2 items-center">
             {/* icones */}
-            {cardProps.icons?.map((icon, index) => {
-              // TODO : adicionar links para os respectivos icones
-              return (
-                <Icon
-                  key={index}
-                  style="w-6 select-none py-1 md:py-2"
-                  Image={icon}
-                  alt="tech icon"
-                  link={
-                    cardProps.iconLinks ? cardProps.iconLinks[index] : undefined
-                  }
-                />
-              );
-            })}
+            <div className="flex items-center px-2 gap-2">
+              {cardProps.icons?.map((icon, index) => {
+                // TODO : adicionar links para os respectivos icones
+                return (
+                  <Icon
+                    key={index}
+                    style="h-8 md:h-10 select-none py-1 md:py-2"
+                    Image={cardProps.iconLinks[index]}
+                    title={icon}
+                    alt="tech icon"
+                  />
+                );
+              })}
+            </div>
+            <div className="hidden lg:block">
+              {!!cardProps.github_link && (
+                <a href={cardProps.github_link}>
+                  <button className="bg-primary rounded-xl px-3 md:w-max w-full py-2 md:py-1 flex items-center justify-center hover:bg-primaryDark transition-colors">
+                    {/* Github icon */}
+                    <Icon style="w-7" Image={GithubDarkIcon} alt="Icone github" title="Github"/>
+
+                    <p className="font-monteserrat font-semibold px-2 hidden md:block">
+                      Ver no Github
+                    </p>
+                  </button>
+                </a>
+              )}
+            </div>
           </div>
         </div>
 
-        <p className="font-poppins text-white text-sm w-full px-2 md:px-0 md:w-4/5 text-start">
+        <p className="font-poppins text-white text-sm w-full px-2 md:px-0 md:w-4/5 text-start my-3">
           {cardProps.content}
         </p>
-      </div>
 
-      <div>
-        {!!cardProps.github_link && (
-          <a href={cardProps.github_link}>
-            <button className="bg-primary rounded-xl px-3 md:w-max w-full py-2 md:py-1 flex items-center justify-center hover:bg-primaryDark transition-colors">
-              {/* Github icon */}
-              <Icon style="w-7" Image={GithubDarkIcon} alt="Icone github" />
+        <div className="block lg:hidden my-1">
+          {!!cardProps.github_link && (
+            <a href={cardProps.github_link}>
+              <button className="bg-primary rounded-xl px-3 md:w-max w-full py-2 md:py-1 flex items-center justify-center hover:bg-primaryDark transition-colors">
+                {/* Github icon */}
+                <Icon style="w-7" Image={GithubDarkIcon} alt="Icone github" title="Github"/>
 
-              <p className="font-monteserrat font-semibold px-2 ">
-                Ver no Github
-              </p>
-            </button>
-          </a>
-        )}
+                <p className="font-monteserrat font-semibold px-2">
+                  Ver no Github
+                </p>
+              </button>
+            </a>
+          )}
+        </div>
       </div>
     </div>
   );
